9fd338e1022f8dcaf81828e6dfb4fa5dd468ae76
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-11-13 11:33:20 +0100

    loading

--- src/app/components/equipo-component/equipo-component.ts
+++ src/app/components/equipo-component/equipo-component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { DatosEquipo } from '../../models/datosEquipo';
 import { ServiceFutbol } from '../../services/ServiceFutbol';
+import { forkJoin } from 'rxjs';
+import { delay } from 'rxjs/operators';
+import { Equipo } from '../../models/equipo';
+import { Jugador } from '../../models/jugador';
 
 @Component({
   selector: 'app-equipo',
@@ -10,26 +14,41 @@ import { ServiceFutbol } from '../../services/ServiceFutbol';
   styleUrl: './equipo-component.css',
 })
 export class EquipoComponent implements OnInit {
-  public dataequipo!: any;
+  public dataequipo!: DatosEquipo | any;
+
+  public equipo?: Equipo;
+  public jugadores?: Array<Jugador>;
+
+  public equipos?: Array<Equipo>;
 
   constructor(private _activateRoute: ActivatedRoute, private _service: ServiceFutbol) {}
 
   ngOnInit(): void {
     this._activateRoute.params.subscribe((params: Params) => {
       let idEquipo = parseInt(params['idequipo']);
-      let datos: DatosEquipo;
-      datos = new DatosEquipo();
+      let datos: DatosEquipo = new DatosEquipo();
 
-      this._service.findEquipo(idEquipo).subscribe((response) => {
-        response.descripcion = response.descripcion.substring(0, 250);
-        datos.equipo = response;
+      forkJoin({
+        equipoReq: this._service.findEquipo(idEquipo),
+        // add artificial delay so loading can be seen in the UI
+        jugadoresReq: this._service.getJugadoresEquipo(idEquipo).pipe(delay(2000)),
+      }).subscribe(({ equipoReq, jugadoresReq }) => {
+        if (equipoReq && equipoReq.descripcion) {
+          equipoReq.descripcion = equipoReq.descripcion.substring(0, 250);
+        }
+
+        datos.equipo = equipoReq;
+        datos.jugadores = jugadoresReq;
+
+        this.equipo = equipoReq;
+        this.jugadores = jugadoresReq;
+
+        this.dataequipo = datos;
       });
 
-      this._service.getJugadoresEquipo(idEquipo).subscribe((response) => {
-        datos.jugadores = response;
+      this._service.getEquipos().subscribe((response) => {
+        this.equipos = response;
       });
-
-      this.dataequipo = datos;
     });
   }
 }
